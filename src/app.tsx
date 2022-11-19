ff6877425d023034bd13e7a8026e7de2ba28cb2f
HUYTHANG\huyth <huythang0903@gmail.com>
2022-11-19 17:27:51 +0700

    update

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>React Application huythang huythang</div>;
+  return <div>React Application huytfajshkdfhang huythang</div>;
 };
 
 export default App;
