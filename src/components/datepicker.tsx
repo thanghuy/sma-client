212b9a28efd0b265ccc3b490937f3c71f9bfdf2b
HUYTHANG\huyth <huythang0903@gmail.com>
2022-12-11 14:28:46 +0700

    update

--- src/components/datepicker.tsx
+++ src/components/datepicker.tsx
@@ -1,39 +1,62 @@
-import React, { useState } from 'react';
-import type { DatePickerProps, TimePickerProps } from 'antd';
-import { DatePicker, Select, Space, TimePicker } from 'antd';
+import React from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
 
-const { Option } = Select;
+type MenuItem = Required<MenuProps>['items'][number];
 
-type PickerType = 'time' | 'date';
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
 
-const PickerWithType = ({
-  type,
-  onChange,
-}: {
-  type: PickerType;
-  onChange: TimePickerProps['onChange'] | DatePickerProps['onChange'];
-}) => {
-  if (type === 'time') return <TimePicker onChange={onChange} />;
-  if (type === 'date') return <DatePicker onChange={onChange} />;
-  return <DatePicker picker={type} onChange={onChange} />;
-};
+const items: MenuProps['items'] = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
+    getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
+  ]),
+
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+
+  getItem('Group', 'grp', null, [getItem('Option 13', '13'), getItem('Option 14', '14')], 'group'),
+];
 
 const DatePickerTest: React.FC = () => {
-  const [type, setType] = useState<PickerType>('time');
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+  };
 
   return (
-    <Space>
-      <Select value={type} onChange={setType}>
-        <Option value="time">Time</Option>
-        <Option value="date">Date</Option>
-        <Option value="week">Week</Option>
-        <Option value="month">Month</Option>
-        <Option value="quarter">Quarter</Option>
-        <Option value="year">Year</Option>
-        <Option value="year">huythnag</Option>
-      </Select>
-      <PickerWithType type={type} onChange={(value) => console.log(value)} />
-    </Space>
+    <Menu
+      onClick={onClick}
+      style={{ width: 256 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+    />
   );
 };
 
